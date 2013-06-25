70f83f1ddbdce84c2ada61dbc84380a8c9b86b29
Deepak Agnihotri <deepak2007in@gmail.com>
2013-06-25 16:12:48 +0530

    My First Check-in

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Abc");
+            Console.WriteLine("Abcd");
             Console.ReadLine();
         }
     }
